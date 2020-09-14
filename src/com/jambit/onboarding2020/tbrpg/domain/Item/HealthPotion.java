c987baa7757d600eec58b734dc2f0dc1f8a8a6a2
S73FAIV <stefan.willibald@protonmail.com>
2020-09-14 16:18:01 +0200

    add HealthPotion edit Consumable edit ItemGenerator

---
+++ src/com/jambit/onboarding2020/tbrpg/domain/Item/HealthPotion.java
@@ -0,0 +1,18 @@
+package com.jambit.onboarding2020.tbrpg.domain.Item;
+
+import com.jambit.onboarding2020.tbrpg.domain.Player.Player;
+
+public class HealthPotion extends Item implements Consumable {
+
+    public HealthPotion(int sellValue) {
+        this.setName("Heiltrank");
+        this.setLore("Damit du nicht gleich abkrazt...");
+        this.setSellValue(5);
+    }
+
+    @Override
+    public void consume(Player player) {
+        player.setHealthState(player.getHealthState() + 20);
+        System.out.println("Der Spieler wurde um 20 Punkte geheilt.\nAktuelle HP: " + player.getHealthState());
+    }
+}
