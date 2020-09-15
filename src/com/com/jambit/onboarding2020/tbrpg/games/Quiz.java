5c0e403e7f5dfb597d5d03af1e563bb16dc3a7af
Stefanie.Tafertshofer@jambit.com <stefanie.tafertshofer@ku.de>
2020-09-15 14:39:01 +0200

    add first draft of quiz feature

---
+++ src/com/com/jambit/onboarding2020/tbrpg/games/Quiz.java
@@ -0,0 +1,47 @@
+package com.jambit.onboarding2020.tbrpg.games;
+
+import java.util.Scanner;
+
+public class Quiz implements Playable{
+
+    private String quizQuestion;
+    private String quizAnswer;
+
+    //Constructor
+    public Quiz(String quizQuestion, String quizAnswer) {
+        this.quizQuestion = quizQuestion;
+        this.quizAnswer = quizAnswer;
+    }
+
+    //getter & setter
+    public String getQuizQuestion() {
+        return quizQuestion;
+    }
+
+    public void setQuizQuestion(String quizQuestion) {
+        this.quizQuestion = quizQuestion;
+    }
+
+    public String getQuizAnswer() {
+        return quizAnswer;
+    }
+
+    public void setQuizAnswer(String quizAnswer) {
+        this.quizAnswer = quizAnswer;
+    }
+
+
+
+
+
+
+    @Override
+    public void play() {
+
+
+            }
+
+
+
+    }
+
