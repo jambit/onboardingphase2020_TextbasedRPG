fba9632fedf553fd33626eaf3696d679d19e6dcc
Willibald <stefan.willibald@jambit.com>
2020-09-17 12:55:05 +0200

    edit Hallway: add if statements

--- src/com/jambit/onboarding2020/tbrpg/domain/Room/Hallway.java
+++ src/com/jambit/onboarding2020/tbrpg/domain/Room/Hallway.java
@@ -2,21 +2,26 @@ package com.jambit.onboarding2020.tbrpg.domain.Room;
 
 import com.jambit.onboarding2020.tbrpg.domain.Player.Player;
 
+import java.util.Scanner;
+
 public class Hallway extends AbstractRoom {
+
+    Scanner in = new Scanner(System.in);
 
     @Override
     public void enter(Player player) {
         System.out.println("Nicht schlecht, du hast den Raum geschafft\n Du ruhst dich im Gang zwischen den Räumen kurz aus");
-        if (false) { // TODO: 16.09.2020 Eingabe "verwende Heiltrank" überprüfen
+        if (in.nextLine().equalsIgnoreCase("verwende Heiltrank")) {
             if (false) {
             } // TODO: 16.09.2020 überprüfung ob Heiltrank im Inventar
-        } else if (false) { // TODO: 16.09.2020 Eingabe "verwende Fluchttrick" überprüfen
+        } else if (in.nextLine().equalsIgnoreCase("verwende Fluchttrick")) {
             if (false) {
             } // TODO: 16.09.2020 überprüfung ob Fluchttrick im Inventar
-        } else if (false) { // TODO: 16.09.2020 Eingabe "statte Waffe aus" überprüfen
+        } else if (in.nextLine().equalsIgnoreCase("statte Waffe aus")) {
+            // TODO: 17.09.2020 Ausgaben möglicher Waffen
             if (false) {
-            } // TODO: 16.09.2020 überprüfung ob Waffe im Inventar
-        } else if (false) { // TODO: 16.09.2020 Eingabe "lege Waffe ab" überprüfen
+            } // TODO: 17.09.2020 Ausstattung der Waffe und abfrage welche Waffe
+        } else if (in.nextLine().equalsIgnoreCase("lege Waffe ab")) {
         }
     }
 }
