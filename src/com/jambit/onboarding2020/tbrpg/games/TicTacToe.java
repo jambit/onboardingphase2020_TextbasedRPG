151319a79c5f0e6b113f247f9099a730fc1d655f
EvaKallrath <eva.kallrath@jambit.com>
2020-09-17 13:10:16 +0200

    Correct Cherrys comments

--- src/com/jambit/onboarding2020/tbrpg/games/TicTacToe.java
+++ src/com/jambit/onboarding2020/tbrpg/games/TicTacToe.java
@@ -4,8 +4,9 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private final int PCinteger = 5;
-    private final int NPCinteger = 2;
+    private final int PC_INTEGER = 5;
+    private final int NPC_INTEGER = 2;
+    private  final int MAXIMUM_PLAYER_MOVES = 8;
     private int [][] board = new int [3][3];
 
     public void printBoard() {
@@ -20,10 +21,10 @@ public class TicTacToe {
 
     //muss es public sein?
     public char getToken(int i, int j){
-        if(board[i][j] == PCinteger)
+        if(board[i][j] == PC_INTEGER)
             return 'X';
 
-        else if(board[i][j] == NPCinteger)
+        else if(board[i][j] == NPC_INTEGER)
             return 'O';
 
         else
@@ -42,39 +43,39 @@ public class TicTacToe {
     private void pcTip(String place) { //PC is 5 NPC is 2
 
         if (place.equals("links oben") && board[0][0] == 0) {
-            board[0][0] = PCinteger;
+            board[0][0] = PC_INTEGER;
         }
 
         else if (place.equals("links unten") && board[2][0] == 0) {
-            board[2][0] = PCinteger;
+            board[2][0] = PC_INTEGER;
         }
 
         else if (place.equals("links mitte") && board[1][0] == 0) {
-            board[1][0] = PCinteger;
+            board[1][0] = PC_INTEGER;
         }
 
         else if (place.equals("rechts oben") && board[0][2] == 0) {
-            board[0][2] = PCinteger;
+            board[0][2] = PC_INTEGER;
         }
 
         else if (place.equals("rechts unten") && board[2][2] == 0) {
-            board[2][2] = PCinteger;
+            board[2][2] = PC_INTEGER;
         }
 
         else if (place.equals("rechts mitte") && board[1][2] == 0) {
-            board[1][2] = PCinteger;
+            board[1][2] = PC_INTEGER;
         }
 
         else if (place.equals("mitte") && board[1][1] == 0) {
-            board[1][1] = PCinteger;
+            board[1][1] = PC_INTEGER;
         }
 
         else if (place.equals("oben mitte") && board[0][1] == 0) {
-            board[0][1] = PCinteger;
+            board[0][1] = PC_INTEGER;
         }
 
         else if (place.equals("unten mitte") && board[2][1] == 0) {
-            board[2][1] = PCinteger;
+            board[2][1] = PC_INTEGER;
         }
         else {
             exception();
@@ -137,34 +138,34 @@ public class TicTacToe {
     private boolean preventWinning() {
 
         for (int i = 0; i < board.length; i++) { //checks vertical
-            if (board[i][0] + board[i][1] + board[i][2] == PCinteger * 2)
+            if (board[i][0] + board[i][1] + board[i][2] == PC_INTEGER * 2)
                 for (int k = 0; k < board.length; k++)
                     if (board[i][k] == 0) {
-                        board[i][k] = NPCinteger;
+                        board[i][k] = NPC_INTEGER;
                         return true;
                     }
         }
 
         for (int i = 0; i < board.length; i++)  //checks horizontal
-            if (board[0][i] + board[1][i] + board[2][i] == PCinteger * 2)
+            if (board[0][i] + board[1][i] + board[2][i] == PC_INTEGER * 2)
                 for (int k = 0; k < board.length; k++)
                     if (board[k][i] == 0 ) {
-                        board[k][i] = NPCinteger;
+                        board[k][i] = NPC_INTEGER;
                         return true;
                     }
 
-        if (board[0][0] + board[1][1] + board[2][2] == PCinteger * 2)  //checks diagonals
+        if (board[0][0] + board[1][1] + board[2][2] == PC_INTEGER * 2)  //checks diagonals
             for (int i = 0; i < board.length; i++)
                 if (board[i][i] == 0) {
-                    board[i][i] = NPCinteger;
+                    board[i][i] = NPC_INTEGER;
                     return true;
                 }
 
-        if (board[0][2] + board[1][1] + board[2][0] == PCinteger * 2)  //checks diagonals
+        if (board[0][2] + board[1][1] + board[2][0] == PC_INTEGER * 2)  //checks diagonals
             for (int i = 0; i < board.length; i++)
                 for (int k = board.length-1; k >= 0; k--)
                     if (board[i][k] ==0) {
-                        board[i][k] = NPCinteger;
+                        board[i][k] = NPC_INTEGER;
                         return true;
                     }
         return false;
@@ -173,33 +174,33 @@ public class TicTacToe {
     public boolean tipNear() {
 
         for (int i = 0; i < board.length; i++)  //checks horizontal
-            if (board[i][0] + board[i][1] + board[i][2] == NPCinteger)
+            if (board[i][0] + board[i][1] + board[i][2] == NPC_INTEGER)
                 for (int k = 0; k < board.length; k++)
                     if (board[i][k] == 0) {
-                        board[i][k] = NPCinteger;
+                        board[i][k] = NPC_INTEGER;
                         return true;
                     }
 
         for (int i = 0; i < board.length; i++)  //checks vertical
-            if (board[0][i] + board[1][i] + board[2][i] == NPCinteger)
+            if (board[0][i] + board[1][i] + board[2][i] == NPC_INTEGER)
                 for (int k = 0; k < board.length; k++)
                     if (board[k][i] == 0) {
-                        board[k][i] = NPCinteger;
+                        board[k][i] = NPC_INTEGER;
                         return true;
                     }
 
-        if (board[0][0] + board[1][1] + board[2][2] == NPCinteger)  //checks diagonals
+        if (board[0][0] + board[1][1] + board[2][2] == NPC_INTEGER)  //checks diagonals
             for (int i = 0; i < board.length; i++)
                 if (board[i][i] == 0) {
-                    board[i][i] = NPCinteger;
+                    board[i][i] = NPC_INTEGER;
                     return true;
                 }
 
-        if (board[0][2] + board[1][1] + board[2][0] == NPCinteger)  //checks diagonals
+        if (board[0][2] + board[1][1] + board[2][0] == NPC_INTEGER)  //checks diagonals
             for (int i = 0; i < board.length; i++)
                 for (int k = board.length; i >= 0; i--)
                     if (board[i][k] ==0) {
-                        board[i][k] = NPCinteger;
+                        board[i][k] = NPC_INTEGER;
                         return true;
                     }
     return false;
@@ -210,35 +211,35 @@ public class TicTacToe {
     public boolean tryToWin() { //NPC checks if he can win
 
         for (int i = 0; i < board.length; i++) { //checks vertical
-            if (board[i][0] + board[i][1] + board[i][2] == NPCinteger * 2)
+            if (board[i][0] + board[i][1] + board[i][2] == NPC_INTEGER * 2)
                 for (int k = 0; k < board.length; k++)
                     if (board[i][k] == 0) {
-                        board[i][k] = NPCinteger;
+                        board[i][k] = NPC_INTEGER;
                         return true;
                     }
         }
 
 
         for (int i = 0; i < board.length; i++)  //checks horizontal
-            if (board[0][i] + board[1][i] + board[2][i] == NPCinteger * 2)
+            if (board[0][i] + board[1][i] + board[2][i] == NPC_INTEGER * 2)
                 for (int k = 0; k < board.length; k++)
                     if (board[k][i] == 0 ) {
-                        board[k][i] = NPCinteger;
+                        board[k][i] = NPC_INTEGER;
                         return true;
                     }
 
-        if (board[0][0] + board[1][1] + board[2][2] == NPCinteger * 2)  //checks diagonals
+        if (board[0][0] + board[1][1] + board[2][2] == NPC_INTEGER * 2)  //checks diagonals
             for (int i = 0; i < board.length; i++)
                 if (board[i][i] == 0) {
-                    board[i][i] = NPCinteger;
+                    board[i][i] = NPC_INTEGER;
                     return true;
                 }
 
-        if (board[0][2] + board[1][1] + board[2][0] == NPCinteger * 2)  //checks diagonals
+        if (board[0][2] + board[1][1] + board[2][0] == NPC_INTEGER * 2)  //checks diagonals
             for (int i = 0; i < board.length; i++)
                 for (int k = board.length -1; k >= 0; k--)
                     if (board[i][k] ==0) {
-                        board[i][k] = NPCinteger;
+                        board[i][k] = NPC_INTEGER;
                         return true;
                     }
         return false;
@@ -264,28 +265,28 @@ public class TicTacToe {
     public int win () { //-1 PC lose; 1 PC wins; 0 nobody wins;
 
         for (int i = 0; i < board.length; i++) { //checks vertical
-            if (board [i][0] + board [i][1] + board [i][2] == PCinteger * 3)
+            if (board [i][0] + board [i][1] + board [i][2] == PC_INTEGER * 3)
                 return 1;
-            else if ((board [i][0] + board [i][1] + board [i][2]) == NPCinteger * 3)
+            else if ((board [i][0] + board [i][1] + board [i][2]) == NPC_INTEGER * 3)
                 return -1;
         }
 
         for (int i = 0; i < board.length; i++) { //checks horizontal
-            if (board [0][i] + board [1][i] + board [2][i] == PCinteger * 3)
+            if (board [0][i] + board [1][i] + board [2][i] == PC_INTEGER * 3)
                 return 1;
-            else if (board [0][i] + board [1][i] + board [2][i] == NPCinteger * 3)
+            else if (board [0][i] + board [1][i] + board [2][i] == NPC_INTEGER * 3)
                 return -1;
         }
 
-        if (board [0][0] + board [1][1] + board [2][2] == PCinteger * 3)  //checks diagonals
+        if (board [0][0] + board [1][1] + board [2][2] == PC_INTEGER * 3)  //checks diagonals
             return 1;
-        else if (board [0][0] + board [1][1] + board [2][2] == NPCinteger * 3)
+        else if (board [0][0] + board [1][1] + board [2][2] == NPC_INTEGER * 3)
             return -1;
 
 
-        if (board [0][2] + board [1][1]  + board [2][0] == PCinteger * 3)  //checks diagonals
+        if (board [0][2] + board [1][1]  + board [2][0] == PC_INTEGER * 3)  //checks diagonals
             return 1;
-        else if (board [0][2] + board [1][1] + board [2][0] == NPCinteger * 3)
+        else if (board [0][2] + board [1][1] + board [2][0] == NPC_INTEGER * 3)
             return -1;
 
         return 0;
@@ -308,39 +309,26 @@ public class TicTacToe {
 
         String dificulty = getDifficulty();
 
-        int counter = 0;
+        int PlayerMovesCounter = 0;
 
         if (whoBegins()) {
 
             System.out.println("Du darfst anfangen, wo setzt du dein Kreuz? Tippe links oben, links unten, links mitte, mitte, etc.");
-            //Scaner2 ist ein suboptimaler Variablenname. Was spricht gegen "scanner"?
-            Scanner scaner2 = new Scanner(System.in);
-            String place = scaner2.nextLine(); //player tips in first position
-            //Todo: Ihr könnt die oberen zwei Zeilen in eine Methode auslagern und direkt im Parameter von PCtip aufrufen. Bspw. getPlayerMove
+            Scanner scanner = new Scanner(System.in);
+            String place = scanner.nextLine(); //player tips in first position
             pcTip(place);
-            counter++;
+            PlayerMovesCounter++;
         }
         else {
             System.out.println("Dein Gegner darf anfangen");
         }
-        // Todo: Anstelle von Zahlen zu verwenden wäre es cool, wenn ihr eine Konstante einführt. Bspw. MAXIMUM_PLAYER_MOVES
-        //  und die Variable counter in "playerMoveCounter" umbenennen. Dann liest es sich einfacher. :)
-        while (counter < 9) { //PC start
-            // Todo: same here: boord als parameter könnt ihr euch sparen, wenn ihr es als Klassenvariable deklariert
+        while (PlayerMovesCounter <= MAXIMUM_PLAYER_MOVES) { //PC start
+
             NPCtip(dificulty); // NPC sets
             printBoard();
-            counter++;
+            PlayerMovesCounter++;
 
-            if (win() == -1) { //check if somebody won
-                printBoard();
-                System.out.println("Du hast verloren");
-                return;
-            }
-            else if (win() == 1) {
-                printBoard();
-                System.out.println("Glückwunsch, du hast gewonnen!");
-                return;
-            }
+            if (endsGameIfWon()) return;
 
             System.out.println("Wo setzt du dein Kreuz? Tippe links oben, links unten, links mitte, mitte, etc.");
 
@@ -350,27 +338,30 @@ public class TicTacToe {
             String place = scan.nextLine(); //player tips in first position
 
             pcTip(place);
-            counter++;
+            PlayerMovesCounter++;
 
-            //Todo: Das ist der gleiche Code wie in Zeilen 345-354 --> ein eigene Methode bietet sich an
-            if (win() == -1) { //check if somebody won
-                printBoard();
-                System.out.println("Du hast verloren");
-                return;
-            }
-            else if (win() == 1) {
-                printBoard();
-                System.out.println("Glückwunsch, du hast gewonnen!");
-                return;
-            }
+            if (endsGameIfWon()) return;
 
-            if (counter == 8) {  // TODO: 16.09.2020 Untenschieden sometimes doesn´t work 
+            if (PlayerMovesCounter == 8) {  // TODO: 16.09.2020 Untenschieden sometimes doesn´t work
                 System.out.println("Unentschieden!");
                 return;
             }
 
         }
 
+    }
+
+    public boolean endsGameIfWon () {
+        if (win() == -1) { //check if somebody won
+            printBoard();
+            System.out.println("Du hast verloren");
+            return true;
+        } else if (win() == 1) {
+            printBoard();
+            System.out.println("Glückwunsch, du hast gewonnen!");
+            return true;
+        }
+        return false;
     }
 
 }
