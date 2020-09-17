7614d067d0ef6367f2e0a32ed5bac8e95eae463f
Willibald <stefan.willibald@jambit.com>
2020-09-17 15:33:11 +0200

    edited GameEngine for stable Gameloop

--- src/com/jambit/onboarding2020/tbrpg/core/GameEngine.java
+++ src/com/jambit/onboarding2020/tbrpg/core/GameEngine.java
@@ -3,6 +3,7 @@ package com.jambit.onboarding2020.tbrpg.core;
 import com.jambit.onboarding2020.tbrpg.domain.Room.AbstractRoom;
 import com.jambit.onboarding2020.tbrpg.domain.Room.Hallway;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -17,30 +18,19 @@ public class GameEngine {
 
    public void run() throws IOException {
 
-      GameInput in = new GameInput(new InputStreamReader(System.in));
-      System.out.println("Du kannst 'enter' und 'quit' tippen.");
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      String line = "";
 
-      while (in.gameState()) {
+      while (line.equalsIgnoreCase("quit") == false) {
 
          Hallway hallway = new Hallway();
 
          for (AbstractRoom room : rooms) {
-            room.printWelcomeMessage();
-
-            try {
-               if (in.inputRoomdecision()) {
-                  room.enter();
-                  hallway.enter();
-               } else {
-                  break;
-               }
-            } catch (Exception e) {
-               System.out.println(e.getMessage());
-            }
+            room.enter();
+            hallway.enter();
          }
       }
       System.out.println("Spiel beendet.");
       in.close();
    }
-
 }
