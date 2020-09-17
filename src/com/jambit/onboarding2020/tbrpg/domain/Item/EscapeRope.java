d1427595491c9a565d45df329b46a050ddaf745b
Willibald <stefan.willibald@jambit.com>
2020-09-17 12:45:51 +0200

    edit Hallway

---
+++ src/com/jambit/onboarding2020/tbrpg/domain/Item/EscapeRope.java
@@ -0,0 +1,11 @@
+package com.jambit.onboarding2020.tbrpg.domain.Item;
+
+import com.jambit.onboarding2020.tbrpg.domain.Player.Player;
+
+public class EscapeRope implements Consumable {
+
+    @Override
+    public void consume(Player player) {
+        //skip next room?
+    }
+}
--- src/com/jambit/onboarding2020/tbrpg/domain/Room/Hallway.java
+++ src/com/jambit/onboarding2020/tbrpg/domain/Room/Hallway.java
@@ -1,5 +1,7 @@
 package com.jambit.onboarding2020.tbrpg.domain.Room;
 
+import com.jambit.onboarding2020.tbrpg.domain.Item.EscapeRope;
+import com.jambit.onboarding2020.tbrpg.domain.Item.HealthPotion;
 import com.jambit.onboarding2020.tbrpg.domain.Player.Player;
 
 public class Hallway extends AbstractRoom {
@@ -7,16 +9,21 @@ public class Hallway extends AbstractRoom {
     @Override
     public void enter(Player player) {
         System.out.println("Nicht schlecht, du hast den Raum geschafft\n Du ruhst dich im Gang zwischen den Räumen kurz aus");
-        if (false) { // TODO: 16.09.2020 Eingabe "verwende Heiltrank" überprüfen
-            if (false) {
-            } // TODO: 16.09.2020 überprüfung ob Heiltrank im Inventar
-        } else if (false) { // TODO: 16.09.2020 Eingabe "verwende Fluchttrick" überprüfen
-            if (false) {
-            } // TODO: 16.09.2020 überprüfung ob Fluchttrick im Inventar
-        } else if (false) { // TODO: 16.09.2020 Eingabe "statte Waffe aus" überprüfen
-            if (false) {
-            } // TODO: 16.09.2020 überprüfung ob Waffe im Inventar
-        } else if (false) { // TODO: 16.09.2020 Eingabe "lege Waffe ab" überprüfen
+        while (false) { // TODO: 17.09.2020 solange input nicht quit -> wiederholen
+            if (false) { // TODO: 16.09.2020 Eingabe "verwende Heiltrank" überprüfen
+                if (player.getInventory().contains(new HealthPotion())) {
+                    //heiltrank.consume();
+                } // TODO: 16.09.2020 überprüfung ob Heiltrank im Inventar
+            } else if (false) { // TODO: 16.09.2020 Eingabe "verwende Fluchttrick" überprüfen
+                if (player.getInventory().contains(new EscapeRope())) {
+                    //escaperope.consume?? -> macht die implementierung hier sinn? -> realisierung überspringen des  nächsten raumes
+                } // TODO: 16.09.2020 überprüfung ob Fluchttrick im Inventar
+            } else if (false) { // TODO: 16.09.2020 Eingabe "statte Waffe aus" überprüfen
+                if (false) {
+                } // TODO: 16.09.2020 überprüfung ob Waffe im Inventar
+            } else if (false) { // TODO: 16.09.2020 Eingabe "lege Waffe ab" überprüfen
+                player.unequipWeapon();
+            }
         }
     }
 }
