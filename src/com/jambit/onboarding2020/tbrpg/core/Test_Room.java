6a2d03b00b850da60253c46fccc07524e9707462
Stefanie.Tafertshofer@jambit.com <stefanie.tafertshofer@ku.de>
2020-09-22 18:33:43 +0200

    change NPCRoom to force the player to interact 4 times

--- src/com/jambit/onboarding2020/tbrpg/core/Test_Room.java
+++ src/com/jambit/onboarding2020/tbrpg/core/Test_Room.java
@@ -1,14 +1,13 @@
 package com.jambit.onboarding2020.tbrpg.core;
 
-import com.jambit.onboarding2020.tbrpg.domain.Room.NPCRoom;
-import com.jambit.onboarding2020.tbrpg.games.sentenceGenerator.MarkovChain;
+import com.jambit.onboarding2020.tbrpg.domain.Room.NPCRoom_PublicOfficial;
 
 public class Test_Room {
     // TODO: 16.09.2020 Delete this class if not further needed
     public static void main(String[] args) {
 
-        NPCRoom npcRoom = new NPCRoom();
-        npcRoom.enter();
+        NPCRoom_PublicOfficial npcRoomPublicOfficial = new NPCRoom_PublicOfficial();
+        npcRoomPublicOfficial.enter();
 /*
 
 
