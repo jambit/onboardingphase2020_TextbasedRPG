cf0fe5e9721c6d9c07ba26dd2801b4ad27c7dc27
Willibald <stefan.willibald@jambit.com>
2020-09-22 10:34:28 +0200

    add Textfiles for Testing

--- src/com/jambit/onboarding2020/tbrpg/games/sentenceGenerator/MarkovChain.java
+++ src/com/jambit/onboarding2020/tbrpg/games/sentenceGenerator/MarkovChain.java
@@ -63,7 +63,7 @@ public class MarkovChain {
     public static void printRandomSentence() {
         try { //filePath has to be the complete Path from /src.../whatever.txt
             // otherwise Files.readAllBites throws an FileNotFoundException
-            System.out.println(markov("src/com/jambit/onboarding2020/tbrpg/games/sentenceGenerator/blog.txt",
+            System.out.println(markov("src/com/jambit/onboarding2020/tbrpg/games/sentenceGenerator/alice_oz.txt",
                     3, 15));
         } catch (IOException e) {
             e.printStackTrace();
