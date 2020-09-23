845553a26f2582718f94ae8ea77f9e255e4470db
Willibald <stefan.willibald@jambit.com>
2020-09-23 15:46:39 +0200

    add Constructor to EscapeRope

--- src/com/jambit/onboarding2020/tbrpg/domain/Item/EscapeRope.java
+++ src/com/jambit/onboarding2020/tbrpg/domain/Item/EscapeRope.java
@@ -4,6 +4,12 @@ import com.jambit.onboarding2020.tbrpg.core.GameState;
 
 public class EscapeRope extends Item implements Consumable {
 
+    public EscapeRope() {
+        this.setName("Fluchtseil");
+        this.setLore("Für was dass wohl gut ist?");
+        this.setSellValue(5);
+    }
+
     @Override
     public void consume() {
         GameState.getGameStateInstance().escapeRopeActive = true;
